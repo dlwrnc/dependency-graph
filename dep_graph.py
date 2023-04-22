32da5da3b43ed2b603c754bf2399d0c4d95f782d
Deborah Lawrence <dlawrence504@gmail.com>
2023-04-21 20:44:50 -0400

    add documentation to some dep_graph methods

--- dep_graph.py
+++ dep_graph.py
@@ -10,22 +10,35 @@ class CyclicalDependencyException(Exception):
 class DependencyGraph:
 
     def __init__(self, json_filepath):
-        if not os.path.exists(json_filepath):
-            raise FileNotFoundError(f'JSON file {json_filepath} '
-                                    f'does not exist on this system.')
-        self.graph = self.read_json(json_filepath) # graph as an adjacent list
+        self.graph = self.read_json(json_filepath)  # graph as an adjacent list
 
     @staticmethod
     def read_json(json_filepath):
+        """consume a json file and return it as an adjacency list
+        Args:
+            json_filepath (str): filepath of the json to consume
+        Returns:
+            data (dict): the json as an adjacency list
+            """
+        if not os.path.exists(json_filepath):
+            raise FileNotFoundError(f'JSON file {json_filepath} '
+                                    f'does not exist on this system.')
         with open(json_filepath) as json_file:
             data = json.load(json_file)
         return data
 
     def print_graph(self):
+        """Prints the graph in fully-traversed bulletpoint format"""
         for package in list(self.graph.keys()):
             self.traverse_dependencies(package)
 
     def traverse_dependencies(self, package, indent=0, visited=None):
+        """Recursive method to traverse all dependencies.
+        Args:
+            package (str): package to traverse sub-dependencies of
+            indent (int): number of indents for the current package
+            visited (set): set of nodes that have been visited
+            """
         if visited is None:
             visited = set()
         if package in visited:
