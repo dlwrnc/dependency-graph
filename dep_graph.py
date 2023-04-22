bad1a8e0190bb5d6bfd41dbe57bcbfe6002d17a3
Deborah Lawrence <dlawrence504@gmail.com>
2023-04-21 20:10:54 -0400

    change exception names, add workflow, add tests

--- dep_graph.py
+++ dep_graph.py
@@ -2,34 +2,43 @@ import os
 import json
 
 
+class CyclicalDependencyException(Exception):
+    """Exception raised when a cyclical dependency is detected"""
+    pass
+
+
 class DependencyGraph:
 
     def __init__(self, json_filepath):
         if not os.path.exists(json_filepath):
-            raise Exception(f'JSON file {json_filepath}'
-                            f' does not exist on this system.')
-        self.json = self.read_json(json_filepath)
-        for package in list(self.json.keys()):
-            self.print_graph(package)
+            raise FileNotFoundError(f'JSON file {json_filepath} '
+                                    f'does not exist on this system.')
+        self.graph = self.read_json(json_filepath) # graph as an adjacent list
 
-    def read_json(self, json_filepath):
+    @staticmethod
+    def read_json(json_filepath):
         with open(json_filepath) as json_file:
             data = json.load(json_file)
         return data
 
-    def print_graph(self, package, indent=0, visited=None):
+    def print_graph(self):
+        for package in list(self.graph.keys()):
+            self.traverse_dependencies(package)
+
+    def traverse_dependencies(self, package, indent=0, visited=None):
         if visited is None:
             visited = set()
         if package in visited:
-            raise Exception('CIRCULAR DEPENDENCY DETECTED')
+            raise CyclicalDependencyException('CIRCULAR DEPENDENCY DETECTED')
         print(" " * indent, f"- {package}")
         visited.add(package)
-        if package in self.json:
-            dependency_list = self.json[package]
+        if package in self.graph:
+            dependency_list = self.graph[package]
             for item in dependency_list:
-                self.print_graph(item, indent+4, visited)
+                self.traverse_dependencies(item, indent+4, visited)
                 visited.remove(item)
 
 
 if __name__ == '__main__':
-    d = DependencyGraph('/Users/deborahlawrence/Code/dependency-graph/test_files/example.json')
+    d = DependencyGraph('test_files/example.json')
+    d.print_graph()
--- test_dep_graph.py
+++ test_dep_graph.py
@@ -1,16 +1,27 @@
 import unittest
+from dep_graph import DependencyGraph, CyclicalDependencyException
 
 
 class TestDepGraph(unittest.TestCase):
 
-    test_jsons = [
-        'complicated_cycle.json',
-        'cycle.json',
-        'example.json'
-    ]
-
     def test_basic_example(self):
-        pass
+        d = DependencyGraph('test_files/example.json')
+        d.print_graph()
+
+    def test_cycle_raises(self):
+        d = DependencyGraph('test_files/cycle.json')
+        with self.assertRaises(CyclicalDependencyException):
+            d.print_graph()
+
+    def test_complicated_cycle_raises(self):
+        d = DependencyGraph('test_files/complicated_cycle.json')
+        with self.assertRaises(CyclicalDependencyException):
+            d.print_graph()
+
+    def test_bad_file(self):
+        with self.assertRaises(FileNotFoundError):
+            # pound is not a legal filename character
+            d = DependencyGraph('bad_file#')
 
 
 if __name__ == '__main__':
